d12fb567b93e094dc89059c000b14a0ff0bc7337
nico <nicolasrfperez@hotmail.com>
2020-10-19 17:55:26 -0300

    para modificar todo lo de venta

--- routes/venta.js
+++ routes/venta.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 const ventaController = require("../controllers/ventaController.js");
 /* GET users listing. */
-//router.post('/',(req,res,next)=>{req.app.validateUser(req,res,next)},  ventaController.create);
-//router.get('/:id', ventaController.getById);
-router.post('/', ventaController.create);
+router.post('/',(req,res,next)=>{req.app.validateUser(req,res,next)},  ventaController.create);
+router.get('/:id', ventaController.getById);
+//router.post('/', ventaController.create);
 
 module.exports = router;
